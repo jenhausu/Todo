dfd9f180831a2e117f192f22976154a7a848645a
蘇健豪 <jenhausu@osensetech.com>
2022-02-15 10:29:23 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 蘇健豪 on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
